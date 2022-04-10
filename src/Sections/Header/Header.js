0abdc4de12a7c8e5763bdbff47c2c9b445c51d13
pkmazarakis <pkmazarakis@gmail.com>
2022-04-10 12:58:43 -0700

    changing header, adding new backgrounds and adding media queries

--- src/Sections/Header/Header.js
+++ src/Sections/Header/Header.js
@@ -3,11 +3,15 @@ import { Button, Icon, IconButton, Typography } from "@mui/material";
 import React, { useEffect } from "react";
 import CoveTypography from "../../components/CoveTypography";
 import BackgroundPicker from "@mui/icons-material/GridOn";
+import { alpha, styled } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
 import CreateRoom from "@mui/icons-material/Add";
 import { isMobile } from "react-device-detect";
 import Spacer from "react-spacer";
 
 function Header({ handleOpen }) {
+  const matches = useMediaQuery("(min-width:1000px)", { noSsr: true });
+
   return (
     <div
       style={{
@@ -23,37 +27,60 @@ function Header({ handleOpen }) {
         // backgroundColor: "rgba(255, 255, 255, 0.2)",
       }}
     >
-      {isMobile ? (
-        <IconButton onClick={() => handleOpen()}>
-          <CreateRoom />
-        </IconButton>
-      ) : (
-        <Button
-          style={{
-            marginRight: "16px",
-            color: "#000000",
-            alignItems: "center",
-          }}
-          startIcon={<CreateRoom />}
-          onClick={() => handleOpen()}
-        >
-          Create Room
-        </Button>
-      )}
+      <div>
+        {isMobile || !matches ? (
+          <IconButton onClick={() => handleOpen()}>
+            <CreateRoom />
+          </IconButton>
+        ) : (
+          <Button
+            style={{
+              marginRight: "16px",
+              color: "#000000",
+              alignItems: "center",
+              backgroundImage: `linear-gradient(135deg, ${alpha(
+                "#FF5ACD",
+                0.5
+              )} 0%, ${alpha("#FBDA61", 0.5)} 100%)`,
+              zIndex: 1000,
+              // padding: "8px",
+              paddingLeft: "16px",
+              paddingRight: "16px",
+              paddingTop: "8px",
+              paddingBottom: "8px",
+            }}
+            startIcon={<CreateRoom />}
+            onClick={() => handleOpen()}
+          >
+            Create Room
+          </Button>
+        )}
 
-      {isMobile ? (
-        <IconButton onClick={() => handleOpen()}>
-          <BackgroundPicker />
-        </IconButton>
-      ) : (
-        <Button
-          style={{ color: "#000000", alignItems: "center" }}
-          startIcon={<BackgroundPicker />}
-          onClick={() => handleOpen()}
-        >
-          Change Background
-        </Button>
-      )}
+        {isMobile || !matches ? (
+          <IconButton onClick={() => handleOpen()}>
+            <BackgroundPicker />
+          </IconButton>
+        ) : (
+          <Button
+            style={{
+              color: "#000000",
+              alignItems: "center",
+              backgroundImage: `linear-gradient(135deg, ${alpha(
+                "#FF5ACD",
+                0.5
+              )} 0%, ${alpha("#FBDA61", 0.5)} 100%)`,
+              paddingLeft: "16px",
+              paddingRight: "16px",
+              paddingTop: "8px",
+              paddingBottom: "8px",
+            }}
+            startIcon={<BackgroundPicker />}
+            onClick={() => handleOpen()}
+          >
+            Change Background
+          </Button>
+        )}
+      </div>
 
       <div
         style={{
@@ -61,6 +88,7 @@ function Header({ handleOpen }) {
           alignItems: "center",
           justifyContent: "center",
           // alignSelf: "center",
+          zIndex: -3,
           position: "absolute",
           left: 0,
           right: 0,
@@ -69,7 +97,7 @@ function Header({ handleOpen }) {
         }}
       >
         <img
-          src={require("../../assets/StudyCoveLogo.png")}
+          src={require("../../assets/SPLogoTransparent.png")}
           width={40}
           height={40}
           style={{ marginRight: "16px" }}
